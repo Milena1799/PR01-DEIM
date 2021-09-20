9d238734e5e16dad01cc98fd4b29f45ac30d0884
Milena <guerreromilena22@gmail.com>
2021-09-20 12:42:00 +0200

    Ejer_1
    Apartado a

--- New Unity Project/Assets/Script/prueba1.cs
+++ New Unity Project/Assets/Script/prueba1.cs
@@ -6,17 +6,21 @@ public class prueba1 : MonoBehaviour
 {
 
     public string texto = "Holi";
+    public string texto1 = "Tus decimales son ";
     public int numeroEntero = 345;
     public int numeroEnteroDos = 3;
     float numeroDecimal = 55.3f;
-    public int resultado; 
-    bool boleana = true;
+    private int resultado; 
+    string cadena;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(texto);
         resultado = numeroEntero * numeroEnteroDos;
         Debug.Log(resultado);
+
+        cadena = texto1 + numeroDecimal;
+        Debug.Log(cadena);
         
     }
 
